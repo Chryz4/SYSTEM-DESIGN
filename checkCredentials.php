751a3e9a8a788810c3f2f02cc56aab0600a352cf
Chryz4 <christellamoses4@gmail.com>
2025-01-26 13:11:07 +0530

    Add files via upload

--- checkCredentials.php
+++ checkCredentials.php
@@ -16,24 +16,37 @@ if ($conn->connect_error) {
 // Get email and password from form
 $email = $_POST['email'];
 $password = $_POST['password'];
+echo $email;
+echo $password;
 
-// Prepare and execute SQL statement
-$stmt = $conn->prepare("SELECT * FROM student_registration WHERE email = ? AND password = ?");
-$stmt->bind_param("ss", $email, $password);
-$stmt->execute();
+// Prepare and execute SQL statement for student
+$stmt_student = $conn->prepare("SELECT * FROM student_registration WHERE email = ? AND password = ?");
+$stmt_student->bind_param("ss", $email, $password);
+$stmt_student->execute();
+$result_student = $stmt_student->get_result();
 
-$result = $stmt->get_result();
+if ($result_student->num_rows > 0) {
+    // Redirect to student portal page
+    header("Location: file:///C:/wamp64/www/StudentPortal.html");
+    exit();
+}
 
-// Check if a row was returned
-if ($result->num_rows > 0) {
-    // Redirect to studentportal page
-    header("Location: C:/wamp64/www/StudentPortal.html");
+// Prepare and execute SQL statement for admin
+$stmt_admin = $conn->prepare("SELECT * FROM admin_details WHERE Email = ? AND Password = ?");
+$stmt_admin->bind_param("ss", $email, $password);
+$stmt_admin->execute();
+$result_admin = $stmt_admin->get_result();
+
+if ($result_admin->num_rows > 0) {
+    // Redirect to admin portal page
+    header("Location: file:///C:/wamp64/www/ADMENU.html ");
     exit();
 } else {
     // Display error message (e.g., incorrect credentials)
     echo "Invalid email or password.";
 }
 
-$stmt->close();
+$stmt_student->close();
+$stmt_admin->close();
 $conn->close();
 ?>
