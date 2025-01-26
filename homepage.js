ae56d8742e6e2a1043ca8b60b61b9d1128885453
Chryz4 <christellamoses4@gmail.com>
2025-01-26 13:14:02 +0530

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,29 @@
+new Swiper('.card-wrapper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    //pagination bullet
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
